ec3a4c41211062983712152af3b7fefe17780157
Falko Schumann <falko.schumann@muspellheim.de>
2013-11-24 23:12:41 +0100

    Backendmodul einfach im Menü hinten anhängen.
    Die genaue Positionierung mit array_insert und vorgegebenen Index ist
    nicht kontrollierbar. Sie ist abhängig von der Anzahl und den Namen der
    installierten Erweiterungen. Grund dafür ist, dass die Erweiterungen
    nach Namen sortiert aus dem modules-Verzeichnis geladen werden.

--- system/modules/bulletin_board/config/config.php
+++ system/modules/bulletin_board/config/config.php
@@ -60,14 +60,11 @@
  * have to be set. Take a look at the system/modules/core/config/config.php
  * file to see how back end modules are configured.
  */
-array_insert($GLOBALS['BE_MOD']['content'], 6, array
+$GLOBALS['BE_MOD']['content']['bulletin_board'] = array
 (
-	'bulletin_board' => array
-	(
-		'tables'       => array('tl_bb_forum'),
-		'icon'         => 'system/modules/bulletin_board/assets/icon.png'
-	)
-));
+	'tables'       => array('tl_bb_forum'),
+	'icon'         => 'system/modules/bulletin_board/assets/icon.png',
+);
 
 
 /**
